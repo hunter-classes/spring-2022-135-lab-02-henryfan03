a8d175f4f4010d4d7f5df8eebaac48d5b095f4c8
Henry Fan <henry.fan2313@gmail.com>
2022-02-04 23:46:50 -0500

    committed edit-array.cpp

---
+++ edit-array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+  int myData[10];
+  for (int i = 0;i<10;i++) {
+    myData[i] = 1;
+  }
+  int index = 0;
+  int value = 0;
+  do {
+    std::cout << "myData = ";
+    for (int i = 0;i<10;i++) {
+      std::cout << myData[i] << " ";
+    }
+    std::cout << std::endl << std::endl;
+    std::cout << "Enter an index value: ";
+    std::cin >> index;
+    std::cout << "Enter an new array value: ";
+    std::cin >> value;
+    myData[index] = value;
+  } while (0 <= index && index <= 10);
+  return 0;
+}
